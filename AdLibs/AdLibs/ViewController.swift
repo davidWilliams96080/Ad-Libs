88059667ed691519b342010513cda6673940de71
David Williams <burmesedragon@att.net>
2020-02-26 11:49:12 -0800

    started on storyboard layout

---
+++ AdLibs/AdLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdLibs
+//
+//  Created by David Williams on 2/26/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
